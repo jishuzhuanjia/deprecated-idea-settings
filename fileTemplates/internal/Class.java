4d6b6371eeba71a8624f3d61904e2a0585646efa
mfcdebugger <mfcdebugger@163.com>
2020-11-24 16:53:10 +0800

    IU-2020.1 <Administrator@OHPG9R1V35BH490 Create baseRefactoring.xml, ide.general.xml, git.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, DevZj.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, package-info.java, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, mavenVersion.xml, templates.xml, AnnotationType.java, Windows copy.xml, Windows copy Proper Redo copy.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, wsl.distributions.xml, Windows copy Proper Redo.xml, vcs.xml, grazie_global.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,14 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+#parse("File Header.java")
+
+/* @author: zhoujian
+ * @qq: 2025513
+ * @create-time: ${DATE} ${TIME}
+ * @description:  
+ * @version: 1.0
+ * @finished: false
+ * @finished-time: 
+ */
+public class ${NAME} {
+}
---
+++ fileTemplates/internal/package-info.java
@@ -0,0 +1,8 @@
+#parse("File Header.java")
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+/**
+ * @author: ${qq}
+ * @test-list: 
+ * 1.${test}
+ * */
